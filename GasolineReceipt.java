544b0753831ed41303b7ca3a52e3e10bd5da42d0
wodham <odhamii@hotmail.com>
2017-01-09 21:34:45 -0500

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main ( String [] args ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      Corner Store      |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 2017-01-07  06:01AM    |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:       10.870  |" );
+    System.out.println( "| Price/gallon  $ 2.159  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel total:  $ 23.47   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
