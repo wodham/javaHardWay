0073a36baf933a96d739cc07288a55c3c22634df
wodham <odhamii@hotmail.com>
2017-01-11 22:30:59 -0500

    CreatingVariables.java

---
+++ CreatingVariables.java
@@ -0,0 +1,32 @@
+public class CreatingVariables {
+  public static void main( String[] args ) {
+    int x, y, z, age;
+    double seconds, e, checking, pie;
+    String firstName, lastName, title, pieTitle;
+
+    x = 10;
+    y = 400;
+    age = 39;
+    z = 1;
+
+    seconds = 4.71;
+    e = 2.71828182845904523536;
+    checking = 1.89;
+    pie = 3.14159;
+
+    firstName = "William";
+    lastName = "Odham";
+    title = "Mr.";
+    pieTitle = "pie";
+
+    System.out.println( "The variable x contains " + x );
+    System.out.println( "The value " + y + " is stored in the variable y." );
+    System.out.println( "The experiment took " + seconds + " seconds. ");
+    System.out.println( "A favorite irrational # is Euler's number: " +e );
+    System.out.println( "Another would be " + pie + " which is " + pieTitle + "!");
+    System.out.println( "Hopefully you have more than $" + checking + "!" );
+    System.out.println( "That would be $" + z + " more than I have.");
+    System.out.println( "My name's " + title + " " + firstName + lastName );
+
+  }
+}
